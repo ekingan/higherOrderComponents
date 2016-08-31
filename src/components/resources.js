49d884c9862ad7fc1e2879f799c670b534d1e322
ekingan <ekingan@gmail.com>
2016-08-31 16:28:50 -0700

    initial

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			Secret Recipe
+			<ul>
+				<li> sugar </li>
+				<li> salt </li>
+				<li> butter </li>
+				<li> popcorn </li>
+			</ul>
+		</div>
+	);
+}
